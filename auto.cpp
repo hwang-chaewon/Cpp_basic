44e7dcff8f2b7800987dc1d09f0c3e8a00967460
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-01 19:47:39 +0900

    Add files via upload

---
+++ auto.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	연산자
+	: +, -, *, /, %
+	*/
+
+	/*
+	auto: C++만의 데이터형
+	초기화하는 값을 보고 데이터형을 C++가 자동으로 결정
+	*/
+	auto n = 100;
+	auto x = true;
+
+	return 0;
+}
