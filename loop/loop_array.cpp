76c9ebdee8632ee65ec648a217e71ac508c4fc2a
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-03 00:08:05 +0900

    Add files via upload

---
+++ loop/loop_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	배열 기반 반복문
+	*/
+	int a[5] = { 1,3,4,5,6 };
+	for (int i : a) {
+		cout << i;
+	}
+
+	/*
+	2차원 배열을 중첩 loop로 접근
+	*/
+
+	return 0;
+}
