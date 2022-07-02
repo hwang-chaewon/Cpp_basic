e357e807891cb556ad3d967f980269e80f14d1b1
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-02 22:11:49 +0900

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	for문의 순서
+		1)int i=0
+		2)i<5
+		3)반복문 내용 수행
+		4)i++
+	*/
+	for (int i = 0; i < 5; i++) {
+		cout << i << "번째입니다";
+	}
+
+	/*
+	증감 연산자
+	++, --
+	*/
+
+	/*
+	관계표현식
+	<, <=, >, >=, ==, !=
+	*/
+	return 0;
+}
