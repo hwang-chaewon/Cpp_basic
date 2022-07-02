9d4219d7e69a26f1b772e85afffc39ede28480f8
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-03 00:18:39 +0900

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	/*
+	if
+	*/
+
+	/*
+	if else
+	*/
+	if (true) {
+		cout << "Âü";
+	}
+	else if (true) {
+		cout << "Âü";
+	}
+	else {
+		cout << "°ÅÁþ";
+	}
+	return 0;
+}
