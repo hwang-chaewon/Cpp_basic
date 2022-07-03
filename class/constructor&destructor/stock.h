1328332528ff3595a279047d03e562e13556bcd7
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-03 22:40:06 +0900

    Add files via upload

---
+++ class/constructor&destructor/stock.h
@@ -0,0 +1,28 @@
+#ifndef STOCK
+#define STOCK
+#include <iostream>
+
+
+using namespace std;
+
+class Stock
+{
+private:
+	string name;
+	int shares;
+	float share_val;
+	double total_val;
+	void set_total() { total_val = shares * share_val; }
+
+public:
+	void buy(int, float);
+	void sell(int, float);
+	void update(float);
+	void show();
+	//持失切
+	Stock(string,int,float);
+	//奄沙持失切
+	Stock();
+	~Stock();
+};
+#endif // !STOCK
